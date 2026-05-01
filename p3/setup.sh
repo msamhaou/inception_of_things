3ffe68e011bbdb2e91f20afe764930179737b9a2
msamhaou <tahaexo@gmail.com>
2026-05-01 19:41:24 +0100

    add : setup script p3

---
+++ p3/setup.sh
@@ -0,0 +1,14 @@
+sudo apt update && sudo apt install curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/bin
+alias k=kubectl
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d cluster create mycluster --kubeconfig-update-default
+kubectl create ns argocd
+kubectl create ns dev
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
