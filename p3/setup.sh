e7528e6eccac27bd0225a93391135e327f734acb
msamhaou <tahaexo@gmail.com>
2026-05-02 19:08:47 +0100

    fix: command chain fix

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,14 +1,28 @@
+export _USER=vagrant
 sudo apt-get update && sudo apt-get install -y curl
+#docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo usermod -aG docker $USER 
+sudo usermod -aG docker $_USER
+#kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 sudo mv kubectl /usr/bin
-alias k=kubectl
+echo "alias k=kubectl" >> ~/.bashrc
+
+#k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-k3d cluster create mycluster --kubeconfig-update-default -p "443:443@loadbalancer"
+sudo k3d cluster create mycluster -p "443:443@loadbalancer"
+
+#kubeconf
+mkdir /home/$_USER/.kube
+sudo k3d kubeconfig get mycluster > /home/$_USER/.kube/config.yaml
+sudo chown -R $_USER:$_USER /home/$_USER/.kube
+export KUBECONFIG=/home/$_USER/.kube/config.yaml
+
 kubectl create ns argocd
 kubectl create ns dev
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+echo "export KUBECONFIG=/home/$_USER/.kube/config.yaml" >> /home/$_USER/.bashrc
+source /home/$_USER/.bashrc
